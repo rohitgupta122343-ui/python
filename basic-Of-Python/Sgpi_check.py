c0e66f8467d95b9c823101adaca6558442714d3e
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-11 06:42:35 -0700

    Create Sgpi_check.py

---
+++ basic-Of-Python/Sgpi_check.py
@@ -0,0 +1,26 @@
+
+sgpi = float(input("enter a sgpi : "))
+
+if 9.00 <= sgpi <= 10.00:
+    print("O Grade ")
+
+elif 8.00 <= sgpi <= 8.99:
+    print("A+ Grade ")
+
+elif 7.00 <= sgpi <= 7.99:
+    print("A Grade ")        
+
+elif 6.00 <= sgpi <= 6.99:
+    print("B+ Grade ")        
+
+elif 5.50 <= sgpi <= 5.99:
+    print("b Grade ") 
+
+elif 5.00 <= sgpi <= 5.49:
+    print("b Grade ")           
+
+elif 4.00 <= sgpi <= 4.99:
+    print("Pass ")   
+
+else:
+    print("Fail ")
