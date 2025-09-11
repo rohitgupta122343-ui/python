e751f0735dbe9a06660132757c5757e7b5814a8e
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-11 03:07:51 -0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,23 @@
+
+
+# list1 = [1,2,3,4,5]
+# list2 = [4,6,7,8,9]
+
+
+# for l1 in list1:
+#     for l2 in list2:
+#         if l1 == l2:
+#             print("hai common ")
+#             break
+
+
+
+for i in range(1,5):
+    print(i)
+    for j in range(i):
+        print("*")
+    
+
+    print("\n")    
+        
+
