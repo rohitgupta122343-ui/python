f5df4aa64cc1810dc3344800f02d396ccfed8ec5
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-11 06:23:28 -0700

    added

--- basic-Of-Python/Century_Age _Calculator.py
+++ basic-Of-Python/Century_Age _Calculator.py
@@ -1,7 +1,7 @@
 
-name = input("Enter a name : ")
+name = input("Enter a Name : ")
 age = int(input("Enter a Age : "))
 
 year = 2025 + (100 - age)
 
-print(name,"you will turn 100 this year",year)
+print(name, "you will turn 100 this year",year)
---
+++ basic-Of-Python/Even_Odd.py
@@ -0,0 +1,7 @@
+
+n = int(input("Enter a no : "))
+
+if n%2 == 0:
+    print("Even No : ",n)
+else:
+    print("Odd No : ",n)    
