e6d320c064dbee92b5a6de34af2496e3a541b445
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-11 08:07:27 -0700

    Create Array-Based Mathematical.py
    Write a program to implement mathematical functions on arrays

---
+++ basic-Of-Python/Array-Based Mathematical.py
@@ -0,0 +1,18 @@
+
+import numpy as np 
+
+arr = np.array([1,2,3,4,5])
+
+# ---------- Mathematical Functions ----------
+
+
+print("Original Array:", arr)
+ 
+
+print("Maximum no of array: ",np.max(arr))
+print("Minimum no of array: ", np.min(arr))
+print("Average no of array: ",np.mean(arr))
+print("Sum no of array: ", np.sum(arr))
+print("Square of array: ",np.square(arr))
+print("Sin (in radians): ",np.sin(arr))
+print("Cos (in radians): ", np.cos(arr))
