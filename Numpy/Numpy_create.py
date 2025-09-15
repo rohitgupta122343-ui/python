efd97076d2fd04b663d89ad029ff457ab0da3700
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-15 08:16:04 -0700

    Update Numpy_create.py

--- Numpy/Numpy_create.py
+++ Numpy/Numpy_create.py
@@ -1,8 +1,8 @@
 
 #How do you create a NumPy array from a Python list?
 
-import numpy as np   #Import the NumPy library
+import numpy as np  #Import the NumPy library
 
-arr = np.array([1,2,3,4,5]) 
+arr = np.array([1,2,3,4,5])
 
 print(arr)
