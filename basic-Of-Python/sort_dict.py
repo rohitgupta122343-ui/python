3dc10781c031b8f7cf820aada5c5b82ad538d259
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-17 08:27:35 -0700

    sort_dict
    This Python script demonstrates how to sort a dictionary by its values in both ascending and descending order using lambda functions and the built-in sorted() method. It also helps understand how key-value pairs work when using .items() in a dictionary.

--- basic-Of-Python/sort_dict.py
+++ basic-Of-Python/sort_dict.py
@@ -1,2 +1,16 @@
 
+# Write a Python script to sort (ascending and descending) a dictionary
+# by value.
 
+data = {
+    "apple" : 2,
+    "banana" : 1,
+    "cherry" : 4,
+    "orange" : 3
+}
+
+asc = dict(sorted(data.items(), key = lambda x: x[1]))
+desc = dict(sorted(data.items(), key = lambda x: x[1],reverse=True))
+
+print("Assending : ",asc)
+print("Descending : ",desc)
