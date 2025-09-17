a3edf79e6ef8551bb4d4110b26b89e51886bc63e
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-17 07:55:20 -0700

    Create camper_common_member.py

---
+++ basic-Of-Python/camper_common_member.py
@@ -0,0 +1,16 @@
+
+def check(l1,l2):
+
+    for i in l1:
+        for j in l2:
+            if i == j:
+                print("The common member is:",j)
+                
+            
+    return -1
+
+
+list1 = [1,2,3,4,5]
+list2 = [5,6,7,8,9] 
+
+print(check(list1,list2))
