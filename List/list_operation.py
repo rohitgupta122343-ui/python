0d4c40b639e2ff18d793d258e404cd0db3becaea
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-19 09:21:44 -0700

    Update list_operation.py

--- List/list_operation.py
+++ List/list_operation.py
@@ -8,7 +8,7 @@ list2 = [1]
 
 list.append(99)            # list = [55, 2, 11, 7, 9, 13, 99]
 list.remove(55)            # list = [2, 11, 7, 9, 13, 99]
-list.extend(list2)         # list = [2, 11, 7, 9, 13, 99, 1]
+list.extend(list2)  # list = [2, 11, 7, 9, 13, 99, 1]
 list.sort()                # list = [1, 2, 7, 9, 11, 13, 99]
 list.sort(reverse=True)    # list = [99, 13, 11, 9, 7, 2, 1]
 list.reverse()             # list = [1, 2, 7, 9, 11, 13, 99]
@@ -18,3 +18,4 @@ ans = len(list)            # ans = 7
 
 print(list)
 print("len ", ans)
+
