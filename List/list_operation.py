bf92686e7111b399599883460b26bfc0c4e03e87
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-19 09:26:44 -0700

    Update list_operation.py

--- List/list_operation.py
+++ List/list_operation.py
@@ -1,22 +1,21 @@
 
 
-
 list = [55, 2, 11, 7, 9, 13]
-list2 = [1]
+list2 = [1, 2, 3]
 
-# list All operations
+# List operations
+list.append(99)             # list = [55, 2, 11, 7, 9, 13, 99]
+list.remove(55)             # list = [2, 11, 7, 9, 13, 99]
+list.extend(list2)          # list = [2, 11, 7, 9, 13, 99, 1, 2, 3]
+list.sort()                 # list = [1, 2, 2, 3, 7, 9, 11, 13, 99]
+list.sort(reverse=True)     # list = [99, 13, 11, 9, 7, 3, 2, 2, 1]
+list.reverse()              # list = [1, 2, 2, 3, 7, 9, 11, 13, 99]
+val = list.pop(1)           # removes element at index 1 (value 2)
+list.insert(0, 100)         # list = [100, 1, 2, 3, 7, 9, 11, 13, 99]
 
-list.append(99)            # list = [55, 2, 11, 7, 9, 13, 99]
-list.remove(55)            # list = [2, 11, 7, 9, 13, 99]
-list.extend(list2)         # list = [2, 11, 7, 9, 13, 99, 1]
-list.sort()                # list = [1, 2, 7, 9, 11, 13, 99]
-list.sort(reverse=True)    # list = [99, 13, 11, 9, 7, 2, 1]
-list.reverse()             # list = [1, 2, 7, 9, 11, 13, 99]
-val = list.pop(1)          # removes 2; list = [1, 7, 9, 11, 13,99]
-list.insert(0, 100)        # list = [100, 1, 2, 7, 9, 11, 13]
-ans = len(list)            # ans = 7
+# results
+ans = len(list)
 
-print(list)
-print("len ", ans)
-print("pop val : ",val)
-
+print("Final list:", list)
+print("Length of list:", ans)
+print("Popped value:", val)
