37063a493a4cbc960e57b4fd4f15411c555adc21
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-22 02:37:26 -0700

    Create 2d_and_3d_array_slicing.py
    This project demonstrates basic indexing techniques in 2D and 3D arrays using Python. It includes examples using both NumPy arrays and standard Python nested lists, making it a helpful reference for beginners learning array data structures and multidimensional indexing.

---
+++ Numpy/2d_and_3d_array_slicing.py
@@ -0,0 +1,31 @@
+
+
+import numpy as np 
+
+# 2D Array
+
+arr2D = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+print("arr2D[1][0]:",arr2D[1][0])  
+# arr2D[1] accesses the second row of the array: [4, 5, 6]
+# arr2D[1][0] accesses the first element of that row: 4
+
+# 3D Array 
+
+arr3D = [
+    
+    [[1,2,3],[10,11,12],[13,14,15]],
+    [[4,5,6],[16,17,18],[20,21,22]],
+    [[7,8,9],[24,25,26],[27,28,29]]
+    
+]
+
+print("Arr3D[2][1][1]",arr3D[2][1][1])
+# arr3D[2] accesses the last row [7,8,9] [24,25,26] [27,28,29]
+# arr3D[2][1] accesses the [24,25,26]
+# arr3D[2][1][1] accesses the 25
+ 
