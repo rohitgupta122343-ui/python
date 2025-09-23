29772ec5ddf5a40ca46f928cf63c7238ef9b98ed
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-23 07:48:34 -0700

    Create Prime_No.py

---
+++ Imp_Question_Python/Prime_No.py
@@ -0,0 +1,14 @@
+
+n = int(input("Enter a number: "))
+
+count = 0
+
+for i in range(1,n+1):
+
+    if n % i == 0:
+        count = count + 1
+
+if count == 2:
+    print("Prime Number: ",n)
+else:
+    print("Not a Prime Number: ")    
