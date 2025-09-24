f3669fd3d600701a734fba36b0703ef30e220a22
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-24 07:18:55 -0700

    Create nested_list_matrix.py

---
+++ List/nested_list_matrix.py
@@ -0,0 +1,15 @@
+
+
+my_list = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+print("3x3 Matrix")
+for i in my_list:
+    print(i)
+
+
+print(my_list[0][1]) # first row , second column --> 2
+print(my_list[1][0]) # second row , first column --> 4
