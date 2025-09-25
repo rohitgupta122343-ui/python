e720208a5f4ac6f5df60e2641e5e58590e8bb6f4
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-25 02:09:13 -0700

    Added

---
+++ Imp_Question_Py/Moduel_1/GradeClassification.py
@@ -0,0 +1,20 @@
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("A+ Grade")
+
+elif marks >= 80:
+    print("A Grade")
+
+elif marks >= 70:
+    print("B Grade")
+
+elif marks >= 60:
+    print("C Grade")            
+
+elif marks >= 50:
+    print("Pass ")
+
+else:
+    print("Fail ")
