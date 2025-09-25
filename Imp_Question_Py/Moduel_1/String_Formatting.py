ff72dac90e3fad4198b47c22cb87cb46903690f2
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-25 01:46:21 -0700

    Insert Substring

---
+++ Imp_Question_Py/Moduel_1/String_Formatting.py
@@ -0,0 +1,6 @@
+
+name = "rohit"
+age = 19
+
+print("Name : {}, Age : {}".format(name,age))
+print(f"Name : {name}, Age : {age}")
---
+++ Imp_Question_Py/Moduel_1/insert_substring.py
@@ -0,0 +1,8 @@
+
+str = "Hello World"
+index = 6
+subString = "Beautiful "
+
+new_String = str[:index] + subString + str[index:]
+
+print(new_String)
