7bba4d36d5678b4f42a3dfed3e7c960d59ef7742
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-25 06:52:44 -0700

    Added

---
+++ Imp_Question_Py/Moduel_1/loop_control_demo.py
@@ -0,0 +1,13 @@
+
+for i in range(1,6):
+    
+    if i == 3:
+        break     # exit the loop 
+
+    if i == 4:
+        continue   # skip the current itreations
+
+    if i == 1:
+        pass      # does nothing
+
+    print(i)     
