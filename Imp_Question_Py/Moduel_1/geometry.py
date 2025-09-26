5a30b47d24d1423f60a2273cceb497e587f31834
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-26 07:06:08 -0700

    added

---
+++ Imp_Question_Py/Moduel_1/geometry.py
@@ -0,0 +1,6 @@
+
+def area_of_circle(r):
+    return 3.14 * r * r *r
+
+def area_of_square(s):
+    return s * s
---
+++ Imp_Question_Py/Moduel_1/main.py
@@ -0,0 +1,5 @@
+
+import geometry 
+
+print(geometry.area_of_circle(5))
+print(geometry.area_of_square(2))
