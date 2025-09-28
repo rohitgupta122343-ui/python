5ef30b45f2a438c10cf939d03c3341238616c69d
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-28 07:15:56 -0700

    added

---
+++ Imp_Question_Py/Moduel_2/tuple.py
@@ -0,0 +1,13 @@
+
+tup = (1, 2, 3, 4)
+
+# index
+print("First element: ", tup[0])   # → 1
+print("last element: ", tup[-1])   # → 4
+
+# slicing
+print("tup[0:3]: ", tup[0:3])      # → (1, 2, 3)
+print("tup[0:]: ", tup[0:])        # → (1, 2, 3, 4)
+
+
+
