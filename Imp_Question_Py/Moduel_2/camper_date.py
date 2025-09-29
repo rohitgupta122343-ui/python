81b65a69d79f0598a590a242502695a25300d2d0
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-29 07:09:09 -0700

    added

--- Imp_Question_Py/Moduel_2/camper_date.py
+++ Imp_Question_Py/Moduel_2/camper_date.py
@@ -1,7 +1,7 @@
 from datetime import date
 
-date1 = date(2023, 5, 17)
-date2 = date(2024, 1, 1)
+date1 = date(2025, 5, 17)
+date2 = date(2026, 1, 1)
 
 if date1 < date2:
     print("Earlier date:", date1)
