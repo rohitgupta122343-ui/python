e427b88107f0b717992e178de3d435dc842a181b
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-29 07:27:09 -0700

    added

---
+++ Imp_Question_Py/Moduel_2/read_write-binaryFile.py
@@ -0,0 +1,14 @@
+
+def binary():
+    data = bytearray([1,2,3,4,5])
+    
+    with open("example.bin","wb")as file:
+
+        file.write(data)
+
+    with open("example.bin","rb")as file:
+
+        content = file.read()    
+        print(content)
+
+binary()
