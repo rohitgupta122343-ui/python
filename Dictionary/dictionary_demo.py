d7c23a078b474f9c1d1d00db1a8b86a2e341a17f
Rohit Gupta <rohitgupta122343@gmail.com>
2025-09-30 07:41:28 -0700

    added

---
+++ Dictionary/dictionary_demo.py
@@ -0,0 +1,14 @@
+
+# dictionary is key value pair 
+
+person = {
+    "name" : "rohit",
+    "age" : 19,
+    "city" : "mumbai"
+}
+
+print(person) # print all key values
+
+print(person["name"]) # print only name 
+print(person["city"])
+
