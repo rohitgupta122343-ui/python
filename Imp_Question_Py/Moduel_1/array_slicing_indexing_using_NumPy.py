e6856267c52362528838b4b60e1bb64852be2cb1
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-03 02:21:01 -0700

    array_slicing_indexing_using_NumPy.py

---
+++ Imp_Question_Py/Moduel_1/array_slicing_indexing_using_NumPy.py
@@ -0,0 +1,17 @@
+
+
+arr = [1,2,3,4,5]
+
+print("Original  Array: ",arr)
+
+# ---- indexing -----
+print("array index 0 ",arr[0])
+print("array index 3 ",arr[3])
+print("array index 1 ",arr[1])
+print("array index -1 ",arr[-1])
+
+
+print("\nSlicing the array:")
+print("arr[1:4] =>", arr[1:4])   
+print("arr[:3] =>", arr[:3])     
+print("arr[::2] =>", arr[::2]) 
