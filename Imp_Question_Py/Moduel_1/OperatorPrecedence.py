46b9d2b8aa856dc70fb41e133a7fbe29b7cd4e16
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-18 01:50:06 -0700

    Update OperatorPrecedence.py

--- Imp_Question_Py/Moduel_1/OperatorPrecedence.py
+++ Imp_Question_Py/Moduel_1/OperatorPrecedence.py
@@ -1,7 +1,7 @@
 
 
-result1 = 10 + 5 * 2     # output --> 20
-result2 = (10 + 5) * 2   # output --> 30
+result1 = 10 + 5 * 2     # output ---> 20
+result2 = (10 + 5) * 2   # output ---> 30
 
 print("Without parentheses 10 + 5 * 2 = ",result1)
 print("With parentheses (10 + 5) * 2 = ",result2)
